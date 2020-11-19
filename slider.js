dbef78e94c4ec7b5c43a42696da0c627447980d9
Nejc <me@nkoporec.com>
2020-11-19 19:09:52 +0100

    Set up basic svg element

--- slider.js
+++ slider.js
@@ -4,8 +4,20 @@ class Slider {
     this.selector = selector;
     this.wrapper = document.querySelector(this.selector); 
     this.sliders = sliders;
+    this.width = 600;
+    this.height = 600;
   }
 
   init() {
+    const svgWrap = document.createElement('div');
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+
+    // Basic svg attributes.
+    svg.setAttribute('height', this.width);
+    svg.setAttribute('width', this.height);
+
+    // Append the new svg el.
+    svgWrap.appendChild(svg);
+    this.wrapper.appendChild(svgWrap);
   }
 }
