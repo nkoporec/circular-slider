724611e511880126f818e4c1ad3e2e70478f10cb
Nejc <me@nkoporec.com>
2020-11-19 19:06:58 +0100

    Initial commit

---
+++ slider.js
@@ -0,0 +1,11 @@
+class Slider {
+  
+  constructor({ selector, sliders }) {
+    this.selector = selector;
+    this.wrapper = document.querySelector(this.selector); 
+    this.sliders = sliders;
+  }
+
+  init() {
+  }
+}
